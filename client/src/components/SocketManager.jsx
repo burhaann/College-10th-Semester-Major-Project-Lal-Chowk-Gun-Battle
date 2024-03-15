61aca6f3187ef8e3a944b0051f1db258b2c4440d
Burhaann <burhaann321@gmail.com>
2024-03-15 17:38:30 +0530

    Fix for the socket connection 002

--- client/src/components/SocketManager.jsx
+++ client/src/components/SocketManager.jsx
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 import { io } from "socket.io-client";
 
 export const socket = io(
-  "http://https://college-10th-semester-major-project-lal.onrender.com:3001"
+  "https://college-10th-semester-major-project-lal.onrender.com:3001"
 );
 export const messagesAtom = atom([]);
 
