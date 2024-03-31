7d1528d4a117bf965dc5cbc882bf77bc3d08ebf9
Burhaann <burhaann321@gmail.com>
2024-03-31 22:13:29 +0530

    Updated with Voice Chat Functionality Version 2 - Bug Fix 4

--- client/src/components/SocketManager.jsx
+++ client/src/components/SocketManager.jsx
@@ -2,8 +2,8 @@ import { atom, useAtom } from "jotai";
 import { useEffect } from "react";
 import { io } from "socket.io-client";
 
-// export const socket = io(process.env.URL);
-export const socket = io("http://localhost:3000");
+export const socket = io(process.env.URL);
+// export const socket = io("http://localhost:3000");
 
 export const messagesAtom = atom([]);
 
